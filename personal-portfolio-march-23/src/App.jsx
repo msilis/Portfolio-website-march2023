fb4d4008ebe71cbe0cd3efad4b484e3463339165
msilis <mikssilis@gmail.com>
2023-04-05 23:38:29 +0100

    Landing page design

--- personal-portfolio-march-23/src/App.jsx
+++ personal-portfolio-march-23/src/App.jsx
@@ -1,14 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import style from "./App.module.css";
+import nameImg from "./assets/Miks.png"
+import techStackLogo from "./assets/techStackImg-withText.png"
 
 function App() {
   
 
   return (
-    <div className="App">
-      
+    <div className={style.App}>
+      <div className={style.nameContainer}>
+        <img src={nameImg} alt="Miks Silis" id={style.nameTag}/>
+        <div className={style.bar1}><p className={style.barText}>Projects</p></div>
+        <div className={style.bar2}><p className={style.barText}>About</p></div>
+        <div className={style.bar3}><p className={style.barText}>Contact</p></div>
+        <img src={techStackLogo} alt="Tech Stack - Javascript, Node.js, MongoDB, React, HTML, CSS" id={style.techStackTag} />
+      </div>
       
     </div>
   )
