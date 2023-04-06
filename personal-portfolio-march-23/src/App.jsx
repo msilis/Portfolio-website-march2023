be31cfc96ed6745a5694a1f8259123c0803ab633
msilis <mikssilis@gmail.com>
2023-04-06 20:37:08 +0100

    react-router initial setup

--- personal-portfolio-march-23/src/App.jsx
+++ personal-portfolio-march-23/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react';
+import { useState,  } from 'react';
+import { Routes, Route } from "react-router-dom";
 import style from "./App.module.css";
 import nameImg from "./assets/Miks.png"
 import techStackLogo from "./assets/techStackImg-withText.png"
@@ -9,11 +10,13 @@ function App() {
   return (
     <div className={style.App}>
       <div className={style.nameContainer}>
+        <Routes>
         <img src={nameImg} alt="Miks Silis" id={style.nameTag}/>
         <div className={style.bar1}><p className={style.barText}>Projects</p></div>
         <div className={style.bar2}><p className={style.barText}>About</p></div>
         <div className={style.bar3}><p className={style.barText}>Contact</p></div>
         <img src={techStackLogo} alt="Tech Stack - Javascript, Node.js, MongoDB, React, HTML, CSS" id={style.techStackTag} />
+        </Routes>
       </div>
       
     </div>
